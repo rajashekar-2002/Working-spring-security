1dcca0c93783378370fb5467c21e707b43ca64bd
rajashe <ganigerrajashekar2002@gmail.com>
2024-04-23 16:13:06 +0530

    authentication provider and cerated a bean for userdetails service

--- src/main/java/com/Authentication/security/SpringSecurityConfiguration.java
+++ src/main/java/com/Authentication/security/SpringSecurityConfiguration.java
@@ -1,22 +1,27 @@
 package com.Authentication.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.Authentication.security.model.MyUserDetailsService;
 
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfiguration {
+
+    @Autowired
+    MyUserDetailsService myUserDetailsService;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -35,23 +40,36 @@ public class SpringSecurityConfiguration {
     // in memory authentication
     // https://bcrypt-generator.com/ for online becrypt password encoder
     //1234 password
+    // @Bean
+    // public UserDetailsService userDetailsService(){
+    //     UserDetails normalUser=User.builder()
+    //                                 .username("user")
+    //                                 .password("$2a$12$WgamfuC2oeg1tKiM4p/u6OFA99ffketrd9Qgj9l1z84ukpZy2hLaq")
+    //                                 .roles("USER")
+    //                                 .build();
+
+    //     UserDetails adminUser=User.builder()
+    //                                 .username("admin")
+    //                                 .password("$2a$12$WgamfuC2oeg1tKiM4p/u6OFA99ffketrd9Qgj9l1z84ukpZy2hLaq")
+    //                                 .roles("USER","ADMIN")
+    //                                 .build();
+
+    //     return new InMemoryUserDetailsManager(normalUser,adminUser);
+    // }
+
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails normalUser=User.builder()
-                                    .username("user")
-                                    .password("$2a$12$WgamfuC2oeg1tKiM4p/u6OFA99ffketrd9Qgj9l1z84ukpZy2hLaq")
-                                    .roles("USER")
-                                    .build();
-
-        UserDetails adminUser=User.builder()
-                                    .username("admin")
-                                    .password("$2a$12$WgamfuC2oeg1tKiM4p/u6OFA99ffketrd9Qgj9l1z84ukpZy2hLaq")
-                                    .roles("USER","ADMIN")
-                                    .build();
-
-        return new InMemoryUserDetailsManager(normalUser,adminUser);
+        return myUserDetailsService;
     }
 
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        //specify type of authentication and password encoder
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(myUserDetailsService);
+        return provider;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
--- src/main/java/com/Authentication/security/model/MyUserDetailsService.java
+++ src/main/java/com/Authentication/security/model/MyUserDetailsService.java
@@ -7,9 +7,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.Authentication.security.Repository.MyUserRepository;
 
+@Service
 public class MyUserDetailsService implements UserDetailsService{
 
     @Autowired
