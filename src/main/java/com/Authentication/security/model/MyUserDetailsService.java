e799606e04e8e5978bcce063ab9e4e940a8877d4
rajashe <ganigerrajashekar2002@gmail.com>
2024-04-23 16:00:34 +0530

    created own myuserdetailservice class and created userdetails

---
+++ src/main/java/com/Authentication/security/model/MyUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.Authentication.security.model;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.Authentication.security.Repository.MyUserRepository;
+
+public class MyUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private MyUserRepository myUserRepository;
+
+
+    //search username and return
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<MyUser> user=myUserRepository.findByUsername(username);
+        if(user.isPresent()){
+            MyUser existuser=user.get();
+
+            //build a userdetail object
+            return User.builder()
+                .username(existuser.getUsername())
+                .password(existuser.getPassword())
+                .roles(getRoles(existuser))
+                .build();
+
+        }else{
+            throw new UsernameNotFoundException(username);
+        }
+    }
+
+
+    private String[] getRoles(MyUser existuser) {
+        if(existuser.getRole().isEmpty()){
+            return new String[]{"USER"};
+        }else{
+            return existuser.getRole().split(",");
+        }
+    }
+    
+}
